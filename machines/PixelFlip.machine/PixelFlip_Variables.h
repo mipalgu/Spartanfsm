12c84c1572b0ef1694d9d0f9d2e689e7bfc8dc88
Morgan McColl <morgan.mccoll92@gmail.com>
2018-06-27 20:28:47 +1000

    Added variables to machine.

--- machines/PixelFlip.machine/PixelFlip_Variables.h
+++ machines/PixelFlip.machine/PixelFlip_Variables.h
@@ -3,3 +3,8 @@
 //
 // Automatically created through MiPalCASE -- do not change manually!
 //
+#extern	redOut: out std_logic_vector(7 downto 0);	///<
+#extern	greenOut: out std_logic_vector(7 downto 0);	///<
+#machine	signal pastRed: std_logic_vector(7 downto 0) := "00000000";	///<
+#machine	signal pastGreen: std_logic_vector(7 downto 0) := "00000000";	///<
+#machine	signal i: integer := 0;	///<
