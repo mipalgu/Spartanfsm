0eec36648948462813768d297c9f2283c1a480a4
Morgan McColl <morgan.mccoll92@gmail.com>
2018-07-28 01:23:00 +1000

    Reconnected Transition.

--- machines/CellularAutomaton.machine/State_Initial.h
+++ machines/CellularAutomaton.machine/State_Initial.h
@@ -35,18 +35,22 @@ namespace FSM
                     virtual void perform(CLMachine *, CLState *) const;
                 };
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wzero-length-array"
-                CLTransition *_transitions[0];
-#pragma clang diagnostic pop
+                class Transition_0: public CLTransition
+                {
+                public:
+                    Transition_0(int toState = 4): CLTransition(toState) {}
 
+                    virtual bool check(CLMachine *, CLState *) const;
+                };
+
+                CLTransition *_transitions[1];
 
                 public:
                     Initial(const char *name = "Initial");
                     virtual ~Initial();
 
                     virtual CLTransition * const *transitions() const { return _transitions; }
-                    virtual int numberOfTransitions() const { return 0; }
+                    virtual int numberOfTransitions() const { return 1; }
 
 #                   include "State_Initial_Variables.h"
 #                   include "State_Initial_Methods.h"
