cc833fb7c2fa5683f31769cfe318b5aa5363aeb2
morgan <morgan.mccoll@griffithuni.edu.au>
2019-08-08 15:52:42 +1000

    Added loop in case echo signal does not reset.

--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/State_TriggerSignal.h
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/State_TriggerSignal.h
@@ -55,14 +55,22 @@ namespace FSM
                     virtual bool check(CLMachine *, CLState *) const;
                 };
 
-                CLTransition *_transitions[1];
+                class Transition_1: public CLTransition
+                {
+                public:
+                    Transition_1(int toState = 0): CLTransition(toState) {}
+
+                    virtual bool check(CLMachine *, CLState *) const;
+                };
+
+                CLTransition *_transitions[2];
 
                 public:
                     TriggerSignal(const char *name = "TriggerSignal");
                     virtual ~TriggerSignal();
 
                     virtual CLTransition * const *transitions() const { return _transitions; }
-                    virtual int numberOfTransitions() const { return 1; }
+                    virtual int numberOfTransitions() const { return 2; }
 
 #                   include "State_TriggerSignal_Variables.h"
 #                   include "State_TriggerSignal_Methods.h"
