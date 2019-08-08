a01e4b7153695032f7bc07e704de3ab315ede9e8
morgan <morgan.mccoll@griffithuni.edu.au>
2019-08-08 15:54:54 +1000

    Reordered variable list.

--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_VarRefs.mm
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_VarRefs.mm
@@ -11,7 +11,7 @@ UltrasonicDistanceSensor *_m = static_cast<UltrasonicDistanceSensor *>(_machine)
 
 #extern	&echo: in std_logic = _m->echo: in std_logic;	///<
 #extern	&trigger: out std_logic = _m->trigger: out std_logic;	///<
-#machine	&signal i: unsigned(23 downto 0) = _m->signal i: unsigned(23 downto 0);	///<
 #extern	&distance: out unsigned(8 downto 0) = _m->distance: out unsigned(8 downto 0);	///<
+#machine	&signal i: unsigned(23 downto 0) = _m->signal i: unsigned(23 downto 0);	///<
 
 #pragma clang diagnostic pop
--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
@@ -5,5 +5,5 @@
 //
 #extern	echo: in std_logic;	///<
 #extern	trigger: out std_logic;	///<
-#machine	signal i: unsigned(23 downto 0);	///<
 #extern	distance: out unsigned(8 downto 0);	///<
+#machine	signal i: unsigned(23 downto 0);	///<
