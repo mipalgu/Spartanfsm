1876cb9bb0af91afcf01c1b7229e30bf28bab5db
morgan <morgan.mccoll@griffithuni.edu.au>
2019-09-17 12:40:51 +1000

    Changed distance calculation to include ringlet length.

--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/State_CalculateDistance_OnEntry.mm
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/State_CalculateDistance_OnEntry.mm
@@ -1,1 +1,1 @@
-distance <= (i * 0.00000002 * 34000) / 2; --[count * period (20ns) * speedOfSound (34000cm/s)]/2
+distance <= (3 * i * 0.00000002 * 34000) / 2; --[periods (3/count) * count * period (20ns) * speedOfSound (34000cm/s)]/2
