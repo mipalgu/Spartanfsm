2de8dee0c5b93932d3b9c6b5ccfa24b22cf63355
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-06-20 23:36:52 +1000

    Fixed error in new constant TIMEOUT.

--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
@@ -10,4 +10,4 @@
 #machine	constant SPEED_OF_SOUND: unsigned(15 downto 0) := x"84D0";	///<
 #machine	constant DOUBLE_FREQ: unsigned(27 downto 0) := x"5F5E100";	///<
 #extern	resetEcho: out std_logic;	///<
-#machine	constant TIMEOUT: unsigned(23 downto 0) := "59C284";	///<
+#machine	constant TIMEOUT: unsigned(23 downto 0) := x"59C284";	///<
