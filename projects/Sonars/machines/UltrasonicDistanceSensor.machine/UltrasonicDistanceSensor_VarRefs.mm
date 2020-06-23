114fbeb55dbed818702b4c77bd1cc240dc116ff1
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-06-23 20:57:22 +1000

    Changed size of distance to 8 bit.

--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_VarRefs.mm
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_VarRefs.mm
@@ -11,7 +11,7 @@ UltrasonicDistanceSensor *_m = static_cast<UltrasonicDistanceSensor *>(_machine)
 
 #extern	&echo: in std_logic = _m->echo: in std_logic;	///<
 #extern	&trigger: out std_logic = _m->trigger: out std_logic;	///<
-#extern	&distance: out unsigned(8 downto 0) = _m->distance: out unsigned(8 downto 0);	///<
+#extern	&distance: out unsigned(7 downto 0) = _m->distance: out unsigned(7 downto 0);	///<
 #machine	&signal i: unsigned(23 downto 0) = _m->signal i: unsigned(23 downto 0);	///<
 #machine	&constant SPEED_OF_SOUND: unsigned(15 downto 0) := x"84D0" = _m->constant SPEED_OF_SOUND: unsigned(15 downto 0) := x"84D0";	///<
 #machine	&constant DOUBLE_FREQ: unsigned(27 downto 0) := x"5F5E100" = _m->constant DOUBLE_FREQ: unsigned(27 downto 0) := x"5F5E100";	///<
--- projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
+++ projects/Sonars/machines/UltrasonicDistanceSensor.machine/UltrasonicDistanceSensor_Variables.h
@@ -5,7 +5,7 @@
 //
 #extern	echo: in std_logic;	///<
 #extern	trigger: out std_logic;	///<
-#extern	distance: out unsigned(8 downto 0);	///<
+#extern	distance: out unsigned(7 downto 0);	///<
 #machine	signal i: unsigned(23 downto 0);	///<
 #machine	constant SPEED_OF_SOUND: unsigned(15 downto 0) := x"84D0";	///<
 #machine	constant DOUBLE_FREQ: unsigned(27 downto 0) := x"5F5E100";	///<
