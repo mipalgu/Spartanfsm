4ee55815e617033fd6592f9f8f9efe6905eda80b
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2020-06-23 06:30:23 -0400

    Updated vhd file.

--- projects/Sonars/src/Sonars/SevenSegmentEncoder.vhd
+++ projects/Sonars/src/Sonars/SevenSegmentEncoder.vhd
@@ -2,7 +2,7 @@
 --
 --This is a generated file - DO NOT ALTER.
 --Please use an LLFSM editor to change this file.
---Date Generated: 2020-06-23 02:18 EDT
+--Date Generated: 2020-06-23 06:29 EDT
 --
 
 library IEEE;
@@ -13,7 +13,8 @@ entity SevenSegmentEncoder is
     port (
         clk: in std_logic;
         EXTERNAL_data: in std_logic_vector(3 downto 0);
-        EXTERNAL_sevenSegmentData: out std_logic_vector(6 downto 0)
+        EXTERNAL_sevenSegmentData: out std_logic_vector(6 downto 0);
+        EXTERNAL_busy: out std_logic
     );
 end SevenSegmentEncoder;
 
@@ -27,30 +28,32 @@ architecture LLFSM of SevenSegmentEncoder is
     constant WriteSnapshot: std_logic_vector(2 downto 0) := "101";
     signal internalState: std_logic_vector(2 downto 0) := ReadSnapshot;
     --State Representation Bits
-    constant STATE_Initial: std_logic_vector(3 downto 0) := "0000";
-    constant STATE_SUSPENDED: std_logic_vector(3 downto 0) := "0001";
-    constant STATE_InitialPseudoState: std_logic_vector(3 downto 0) := "0010";
-    constant STATE_Compare: std_logic_vector(3 downto 0) := "0011";
-    constant STATE_Zero: std_logic_vector(3 downto 0) := "0100";
-    constant STATE_One: std_logic_vector(3 downto 0) := "0101";
-    constant STATE_Two: std_logic_vector(3 downto 0) := "0110";
-    constant STATE_Three: std_logic_vector(3 downto 0) := "0111";
-    constant STATE_Four: std_logic_vector(3 downto 0) := "1000";
-    constant STATE_Five: std_logic_vector(3 downto 0) := "1001";
-    constant STATE_Six: std_logic_vector(3 downto 0) := "1010";
-    constant STATE_Seven: std_logic_vector(3 downto 0) := "1011";
-    constant STATE_Eight: std_logic_vector(3 downto 0) := "1100";
-    constant STATE_Nine: std_logic_vector(3 downto 0) := "1101";
-    constant STATE_Wait: std_logic_vector(3 downto 0) := "1110";
-    constant STATE_Error: std_logic_vector(3 downto 0) := "1111";
-    signal currentState: std_logic_vector(3 downto 0) := STATE_Initial;
-    signal targetState: std_logic_vector(3 downto 0) := currentState;
-    signal previousRinglet: std_logic_vector(3 downto 0) := STATE_Initial xor "1111";
+    constant STATE_Initial: std_logic_vector(4 downto 0) := "00000";
+    constant STATE_SUSPENDED: std_logic_vector(4 downto 0) := "00001";
+    constant STATE_InitialPseudoState: std_logic_vector(4 downto 0) := "00010";
+    constant STATE_Compare: std_logic_vector(4 downto 0) := "00011";
+    constant STATE_Zero: std_logic_vector(4 downto 0) := "00100";
+    constant STATE_One: std_logic_vector(4 downto 0) := "00101";
+    constant STATE_Two: std_logic_vector(4 downto 0) := "00110";
+    constant STATE_Three: std_logic_vector(4 downto 0) := "00111";
+    constant STATE_Four: std_logic_vector(4 downto 0) := "01000";
+    constant STATE_Five: std_logic_vector(4 downto 0) := "01001";
+    constant STATE_Six: std_logic_vector(4 downto 0) := "01010";
+    constant STATE_Seven: std_logic_vector(4 downto 0) := "01011";
+    constant STATE_Eight: std_logic_vector(4 downto 0) := "01100";
+    constant STATE_Nine: std_logic_vector(4 downto 0) := "01101";
+    constant STATE_Wait: std_logic_vector(4 downto 0) := "01110";
+    constant STATE_Error: std_logic_vector(4 downto 0) := "01111";
+    constant STATE_Dash: std_logic_vector(4 downto 0) := "10000";
+    signal currentState: std_logic_vector(4 downto 0) := STATE_Initial;
+    signal targetState: std_logic_vector(4 downto 0) := currentState;
+    signal previousRinglet: std_logic_vector(4 downto 0) := STATE_Initial xor "11111";
     --Snapshot of External Variables
     signal data: std_logic_vector(3 downto 0);
     signal sevenSegmentData: std_logic_vector(6 downto 0);
+    signal busy: std_logic;
     --Machine Variables
-    signal oldData: std_logic_vector(3 downto 0);
+    signal latchedData: std_logic_vector(3 downto 0);
 begin
 process (clk)
     begin
@@ -67,6 +70,7 @@ process (clk)
                     case currentState is
                         when STATE_Initial=>
                             sevenSegmentData <= "0000000";
+                            latchedData <= data;
                         when STATE_Zero=>
                             sevenSegmentData <= "1111110";
                         when STATE_One=>
@@ -87,8 +91,12 @@ process (clk)
                             sevenSegmentData <= "1111111";
                         when STATE_Nine=>
                             sevenSegmentData <= "1110011";
+                        when STATE_Wait=>
+                            busy <= '0';
                         when STATE_Error=>
                             sevenSegmentData <= "1001111";
+                        when STATE_Dash=>
+                            sevenSegmentData <= "0000001";
                         when others =>
                             null;
                     end case;
@@ -112,37 +120,40 @@ process (clk)
                                 internalState <= Internal;
                             end if;
                         when STATE_Compare=>
-                            if (data = x"0") then
+                            if (latchedData = x"0") then
                                 targetState <= STATE_Zero;
                                 internalState <= OnExit;
-                            elsif (true) and (not (data = x"0")) then
+                            elsif (latchedData = x"A") and (not (latchedData = x"0")) then
                                 targetState <= STATE_One;
                                 internalState <= OnExit;
-                            elsif (data = x"1") and (not (true)) and (not (data = x"0")) then
+                            elsif (true) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Two;
                                 internalState <= OnExit;
-                            elsif (data = x"2") and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"1") and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Three;
                                 internalState <= OnExit;
-                            elsif (data = x"3") and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"2") and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Four;
                                 internalState <= OnExit;
-                            elsif (data = x"4") and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"3") and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Five;
                                 internalState <= OnExit;
-                            elsif (data = x"5") and (not (data = x"4")) and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"4") and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Six;
                                 internalState <= OnExit;
-                            elsif (data = x"6") and (not (data = x"5")) and (not (data = x"4")) and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"5") and (not (latchedData = x"4")) and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Seven;
                                 internalState <= OnExit;
-                            elsif (data = x"7") and (not (data = x"6")) and (not (data = x"5")) and (not (data = x"4")) and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"6") and (not (latchedData = x"5")) and (not (latchedData = x"4")) and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Eight;
                                 internalState <= OnExit;
-                            elsif (data = x"8") and (not (data = x"7")) and (not (data = x"6")) and (not (data = x"5")) and (not (data = x"4")) and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"7") and (not (latchedData = x"6")) and (not (latchedData = x"5")) and (not (latchedData = x"4")) and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Nine;
                                 internalState <= OnExit;
-                            elsif (data = x"9") and (not (data = x"8")) and (not (data = x"7")) and (not (data = x"6")) and (not (data = x"5")) and (not (data = x"4")) and (not (data = x"3")) and (not (data = x"2")) and (not (data = x"1")) and (not (true)) and (not (data = x"0")) then
+                            elsif (latchedData = x"8") and (not (latchedData = x"7")) and (not (latchedData = x"6")) and (not (latchedData = x"5")) and (not (latchedData = x"4")) and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
+                                targetState <= STATE_Dash;
+                                internalState <= OnExit;
+                            elsif (latchedData = x"9") and (not (latchedData = x"8")) and (not (latchedData = x"7")) and (not (latchedData = x"6")) and (not (latchedData = x"5")) and (not (latchedData = x"4")) and (not (latchedData = x"3")) and (not (latchedData = x"2")) and (not (latchedData = x"1")) and (not (true)) and (not (latchedData = x"A")) and (not (latchedData = x"0")) then
                                 targetState <= STATE_Error;
                                 internalState <= OnExit;
                             else
@@ -219,13 +230,20 @@ process (clk)
                                 internalState <= Internal;
                             end if;
                         when STATE_Wait=>
-                            if (data /= oldData) then
+                            if (latchedData /= data) then
                                 targetState <= STATE_Compare;
                                 internalState <= OnExit;
                             else
                                 internalState <= Internal;
                             end if;
                         when STATE_Error=>
+                            if (true) then
+                                targetState <= STATE_Wait;
+                                internalState <= OnExit;
+                            else
+                                internalState <= Internal;
+                            end if;
+                        when STATE_Dash=>
                             if (true) then
                                 targetState <= STATE_Wait;
                                 internalState <= OnExit;
@@ -243,14 +261,18 @@ process (clk)
                     internalState <= WriteSnapshot;
                 when OnExit =>
                     case currentState is
-                        when STATE_Compare=>
-                            oldData <= data;
+                        when STATE_Initial=>
+                            busy <= '1';
+                        when STATE_Wait=>
+                            busy <= '1';
+                            latchedData <= data;
                         when others =>
                             null;
                     end case;
                     internalState <= WriteSnapshot;
                 when WriteSnapshot =>
                     EXTERNAL_sevenSegmentData <= sevenSegmentData;
+                    EXTERNAL_busy <= busy;
                     internalState <= ReadSnapshot;
                     previousRinglet <= currentState;
                     currentState <= targetState;
