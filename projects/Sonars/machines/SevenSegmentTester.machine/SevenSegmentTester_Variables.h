9deda61542e7c5c0c325459b628e3c0acacffc2f
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-06-29 15:30:58 +1000

    Removed slaveClk machine variable. Separating clk generation into new file.

--- projects/Sonars/machines/SevenSegmentTester.machine/SevenSegmentTester_Variables.h
+++ projects/Sonars/machines/SevenSegmentTester.machine/SevenSegmentTester_Variables.h
@@ -4,9 +4,8 @@
 // Automatically created through MiPalCASE -- do not change manually!
 //
 #extern	dataLine: out std_logic;	///<
-#extern	slaveClk: out std_logic;	///<
+#extern	slaveClk: in std_logic;	///<
 #extern	cs: out std_logic;	///<
-#machine	signal slaveClkLatched: std_logic;	///<
 #machine	signal data: std_logic_vector(7 downto 0);	///<
 #machine	signal digit: unsigned(3 downto 0);	///<
 #machine	signal busy: std_logic;	///<
