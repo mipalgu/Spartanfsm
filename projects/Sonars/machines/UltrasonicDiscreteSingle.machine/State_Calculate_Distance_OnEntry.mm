ed135073f037ef91b9e5711cf12ac8c58a6842dd
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2020-09-05 20:38:27 +1000

    Fixed problem with brackets.

--- projects/Sonars/machines/UltrasonicDiscreteSingle.machine/State_Calculate_Distance_OnEntry.mm
+++ projects/Sonars/machines/UltrasonicDiscreteSingle.machine/State_Calculate_Distance_OnEntry.mm
@@ -1,1 +1,1 @@
-distance <= std_logic_vector(resize(numloops* SCHEDULE_LENGTH / x"3E8" / SPEED_OF_SOUND / x"2710"), 16);
+distance <= std_logic_vector(resize((numloops* SCHEDULE_LENGTH / x"3E8" / SPEED_OF_SOUND / x"2710"), 16));
