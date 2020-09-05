36d231025b0d341c8cd33eaa5766dfaaecfead42
Morgan McColl <morgan.mccoll92@gmail.com>
2020-09-05 20:46:48 +1000

    Created 7Seg project.

---
+++ projects/7SegmentDisplay/top.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity top is
+	port (
+		HEX0: out std_logic_vector(6 downto 0);
+		HEX1: out std_logic_vector(6 downto 0);
+		HEX2: out std_logic_vector(6 downto 0);
+		HEX3: out std_logic_vector(6 downto 0);
+		HEX4: out std_logic_vector(6 downto 0);
+		HEX5: out std_logic_vector(6 downto 0);
+		HEX6: out std_logic_vector(6 downto 0);
+		HEX7: out std_logic_vector(6 downto 0)
+	);
+end top;
