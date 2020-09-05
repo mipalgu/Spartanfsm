653aebef22f3e2452da196b1d54c80a4b5fe28dd
Morgan McColl <morgan.mccoll92@gmail.com>
2020-09-05 20:52:08 +1000

    Added all external vars.

--- projects/7SegmentDisplay/top.vhd
+++ projects/7SegmentDisplay/top.vhd
@@ -1,8 +1,10 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity top is
 	port (
+		CLOCK_50: in std_logic;
 		HEX0: out std_logic_vector(6 downto 0);
 		HEX1: out std_logic_vector(6 downto 0);
 		HEX2: out std_logic_vector(6 downto 0);
@@ -10,6 +12,12 @@ entity top is
 		HEX4: out std_logic_vector(6 downto 0);
 		HEX5: out std_logic_vector(6 downto 0);
 		HEX6: out std_logic_vector(6 downto 0);
-		HEX7: out std_logic_vector(6 downto 0)
+		HEX7: out std_logic_vector(6 downto 0);
+		Key: in std_logic_vector(3 downto 0)
 	);
 end top;
+
+architecture Behavioural of top is
+	
+
+end architecture;
