2dab2da35a35e68cce1ad66b1d0f260a7b3068f9
Morgan McColl <morgan.mccoll92@gmail.com>
2020-09-11 21:14:03 +1000

    Removed src folder.

--- common_machines/SevenSegDisplay/src/digit.vhd
+++
@@ -1,7 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-package digit_array_pkg is
-    type digit_array is array(natural range <>) of std_logic_vector;
-end package;
