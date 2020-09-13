b5cdf926c950599eaa4600e75135429f3c92b910
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2020-09-14 01:17:34 +1000

    Implemented after smenatics.

--- interpretor/main.hs
+++ interpretor/main.hs
@@ -19,13 +19,17 @@ main = do
     initialState <- getInitialState dir states
     internals <- getAllInternals dir states
     transitions <- getAllTransCodeForAllStates dir states
+    afters <- return $ map doesStateHaveAfter transitions
+    putStrLn (show transitions)
+    putStrLn (show states)
+    putStrLn (show afters)
     numberOfTargets <- return $ getNumberOfTargets states transitions
     targets <- getAllTargets dir states numberOfTargets
     targetStates <- return $ allTargetsToState states targets
     stateBitSize <- getNumberOfBits dir
     projectName <- return $ getProjectName dir
     variables <- getVariables dir projectName
-    architecture <- return $ createArchitecture states internals transitions targetStates stateBitSize projectName variables initialState
+    architecture <- return $ createArchitecture states afters internals transitions targetStates stateBitSize projectName variables initialState
     includes <- getIncludes dir projectName
     entity <- return $ createEntity includes projectName variables
     timeString <- getLocalTimeString
@@ -35,22 +39,6 @@ main = do
 --Gets project name by inspecting the folder structure
 getProjectName :: String -> String
 getProjectName dir = head (splitOn ".machine" (last (filter (\x -> x /= "") (splitOn "/" dir))))
-
-
---Creates the VHDL code to check whether a machine has been suspended or not.
-createSuspendedLogic :: String
-createSuspendedLogic =
-    "if (suspended = '1' and restart /= '1' and currentState /= " ++ toStateName suspended ++ ") then"
-    +\-> "suspendedFrom <= currentState;" +\-> "currentState <= " ++ toStateName suspended ++ ";"
-    +\-> "previousInternal <= internalState;" +\-> "internalState <= OnEntry;"
-    +\> "elsif (suspended = '0' and currentState = " ++ toStateName suspended ++ ") then"
-    +\-> "internalState <= previousInternal;" +\-> "currentState <= suspendedFrom;"
-    +\> "elsif (suspended = '1' and restart = '1') then"
-    +\-> "restart <= '0';" +\-> "suspended <= '0';" +\-> "currentState <= " ++ toStateName initialPseudostate ++ ";"
-    +\-> "internalState <= OnEntry;"
-    +\> "elsif (restart = '1' and suspended = '0') then"
-    +\-> "restart <= '0';"
-    +\> "end if;"
 
 --The default comment located at the top of a generated machine .vhd file.
 createMachineComment :: String -> String -> String
