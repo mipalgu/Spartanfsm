f233ee8ef3a263c177ed575e6017f59465d91e25
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-09-15 01:04:50 +1000

    Fixed some errors in machine --  need regeneration.

--- projects/Sonars/machine/SonarPlatform.machine/SonarPlatform_Variables.h
+++ projects/Sonars/machine/SonarPlatform.machine/SonarPlatform_Variables.h
@@ -9,8 +9,8 @@
 #extern	echos: inout std_logic_vector(numberOfSensors - 1 downto 0);	///<
 #machine	signal smallestDistance: std_logic_vector(15 downto 0);	///<
 #machine	constant allSuspended: std_logic_vector(numberOfSensors - 1 downto 0) := (others => '1');	///<
-#machine	signal sensorSuspend: std_logic;	///<
+#machine	signal sensorsSuspended: std_logic_vector(numberOfSensors - 1 downto 0);	///<
 #machine	signal sensorFusionSuspended: std_logic;	///<
-#machine	signal allOutputs: std_logic_vector(numberOfSensors * sensorOutputSize - 1 downto 0);	///<
+#machine	signal allOutputs: std_logic_vector(numberOfSensors * 16 - 1 downto 0);	///<
 #machine	signal sensorCommand: std_logic_vector(1 downto 0) := COMMAND_NULL;	///<
 #machine	signal sensorFusionCommand: std_logic_vector(1 downto 0) := COMMAND_NULL;	///<
