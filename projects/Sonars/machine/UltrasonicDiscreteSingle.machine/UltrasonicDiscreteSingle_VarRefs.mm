146e03ed2c5b7e3f7e4390617361f287a1d3b933
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-09-23 07:20:06 +1000

    Changed offset to 53.

--- projects/Sonars/machine/UltrasonicDiscreteSingle.machine/UltrasonicDiscreteSingle_VarRefs.mm
+++ projects/Sonars/machine/UltrasonicDiscreteSingle.machine/UltrasonicDiscreteSingle_VarRefs.mm
@@ -14,7 +14,7 @@ UltrasonicDiscreteSingle *_m = static_cast<UltrasonicDiscreteSingle *>(_machine)
 #extern	&distance: out std_logic_vector(15 downto 0) = _m->distance: out std_logic_vector(15 downto 0);	///<
 #machine	&constant SCHEDULE_LENGTH: natural := 100 = _m->constant SCHEDULE_LENGTH: natural := 100;	///<
 #machine	&constant SPEED_OF_SOUND: natural := 343 = _m->constant SPEED_OF_SOUND: natural := 343;	///<
-#machine	&constant SONAR_OFFSET: natural := 40 = _m->constant SONAR_OFFSET: natural := 40;	///<
+#machine	&constant SONAR_OFFSET: natural := 53 = _m->constant SONAR_OFFSET: natural := 53;	///<
 #machine	&constant MAX_DISTANCE: natural := 4000000 = _m->constant MAX_DISTANCE: natural := 4000000;	///<
 #machine	&constant MAX_TIME: natural := MAX_DISTANCE * 2 / SPEED_OF_SOUND * 1000 = _m->constant MAX_TIME: natural := MAX_DISTANCE * 2 / SPEED_OF_SOUND * 1000;	///<
 #machine	&shared variable numloops: natural := 0 = _m->shared variable numloops: natural := 0;	///<
--- projects/Sonars/src/UltrasonicDiscreteSingle.vhd
+++ projects/Sonars/src/UltrasonicDiscreteSingle.vhd
@@ -2,7 +2,7 @@
 --
 --This is a generated file - DO NOT ALTER.
 --Please use an LLFSM editor to change this file.
---Date Generated: 2020-09-23 07:18 AEST
+--Date Generated: 2020-09-23 07:19 AEST
 --
 --Author: Morgan McColl
 --Email: morgan.mccoll@alumni.griffithuni.edu.au
@@ -75,7 +75,7 @@ architecture LLFSM of UltrasonicDiscreteSingle is
     --Machine Variables
     constant SCHEDULE_LENGTH: natural := 100;
     constant SPEED_OF_SOUND: natural := 343;
-    constant SONAR_OFFSET: natural := 40;
+    constant SONAR_OFFSET: natural := 53;
     constant MAX_DISTANCE: natural := 4000000;
     constant MAX_TIME: natural := MAX_DISTANCE * 2 / SPEED_OF_SOUND * 1000;
     shared variable numloops: natural := 0;
