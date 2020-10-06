b4f5f78d6625a66014a43b9208075908397e9984
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-10-06 15:31:42 +1000

    regenerated SuspensibleHelloWorld.

--- examples/SuspensibleHelloWorld/src/ParentMachine.vhd
+++ examples/SuspensibleHelloWorld/src/ParentMachine.vhd
@@ -2,7 +2,7 @@
 --
 --This is a generated file - DO NOT ALTER.
 --Please use an LLFSM editor to change this file.
---Date Generated: 2020-09-21 02:22 AEST
+--Date Generated: 2020-10-06 15:30 AEST
 --
 --Author: Morgan McColl
 --Email: morgan.mccoll@alumni.griffithuni.edu.au
@@ -46,13 +46,15 @@ architecture LLFSM of ParentMachine is
     constant STATE_SuspendChild: std_logic_vector(2 downto 0) := "100";
     constant STATE_RestartChild: std_logic_vector(2 downto 0) := "101";
     signal currentState: std_logic_vector(2 downto 0) := STATE_Initial;
-    signal targetState: std_logic_vector(2 downto 0) := currentState;
-    signal previousRinglet: std_logic_vector(2 downto 0) := STATE_Initial xor "111";
+    signal targetState: std_logic_vector(2 downto 0) := STATE_Initial;
+    signal previousRinglet: std_logic_vector(2 downto 0) := "ZZZ";
     signal suspendedFrom: std_logic_vector(2 downto 0) := STATE_Initial;
+    --Suspension Commands
     constant COMMAND_NULL: std_logic_vector(1 downto 0) := "00";
     constant COMMAND_RESTART: std_logic_vector(1 downto 0) := "01";
     constant COMMAND_SUSPEND: std_logic_vector(1 downto 0) := "10";
     constant COMMAND_RESUME: std_logic_vector(1 downto 0) := "11";
+    --After Variables
     shared variable ringlet_counter: natural := 0;
     constant clockPeriod: real := 20.0;
     constant ringletLength: real := 5.0 * clockPeriod;
@@ -66,21 +68,7 @@ architecture LLFSM of ParentMachine is
     signal childCommand: std_logic_vector(1 downto 0);
     signal childsLED: std_logic;
     signal childSuspended: std_logic;
-	 component SuspensibleHelloWorld is
-		port (
-        clk: in std_logic;
-        command: in std_logic_vector(1 downto 0);
-        suspended: out std_logic;
-        EXTERNAL_LED: out std_logic
-		);
-	 end component;
 begin
-	child_gen: SuspensibleHelloWorld port map (
-        clk => clk,
-        command => childCommand,
-        suspended => childSuspended,
-        EXTERNAL_LED => childsLED
-	);
 process (clk)
     begin
         if (rising_edge(clk)) then
@@ -93,20 +81,30 @@ process (clk)
                         targetState <= STATE_Initial;
                         if (previousRinglet = STATE_SUSPENDED) then
                             internalState <= OnResume;
+                        elsif (previousRinglet = STATE_Initial) then
+                            internalState <= NoOnEntry;
                         else
                             internalState <= OnEntry;
                         end if;
                     elsif (command = COMMAND_RESUME and currentState = STATE_SUSPENDED and suspendedFrom /= STATE_SUSPENDED) then
                         suspended <= '0';
                         currentState <= suspendedFrom;
-                        internalState <= OnResume;
                         targetState <= suspendedFrom;
+                        if (previousRinglet = suspendedFrom) then
+                            internalState <= NoOnEntry;
+                        else
+                            internalState <= OnResume;
+                        end if;
                     elsif (command = COMMAND_SUSPEND and currentState /= STATE_SUSPENDED) then
                         suspendedFrom <= currentState;
                         suspended <= '1';
                         currentState <= STATE_SUSPENDED;
-                        internalState <= OnSuspend;
                         targetState <= STATE_SUSPENDED;
+                        if (previousRinglet = STATE_SUSPENDED) then
+                            internalState <= NoOnEntry;
+                        else
+                            internalState <= OnSuspend;
+                        end if;
                     elsif (currentState = STATE_SUSPENDED) then
                         suspended <= '1';
                         if (previousRinglet /= STATE_SUSPENDED) then
@@ -176,12 +174,8 @@ process (clk)
                 when CheckTransition =>
                     case currentState is
                         when STATE_Initial =>
-                            if (true) then
-                                targetState <= STATE_SetLED;
-                                internalState <= OnExit;
-                            else
-                                internalState <= Internal;
-                            end if;
+                            targetState <= STATE_SetLED;
+                            internalState <= OnExit;
                         when STATE_SUSPENDED =>
                             internalState <= Internal;
                         when STATE_SetLED =>
@@ -202,19 +196,11 @@ process (clk)
                                 internalState <= Internal;
                             end if;
                         when STATE_SuspendChild =>
-                            if (true) then
-                                targetState <= STATE_SetLED;
-                                internalState <= OnExit;
-                            else
-                                internalState <= Internal;
-                            end if;
+                            targetState <= STATE_SetLED;
+                            internalState <= OnExit;
                         when STATE_RestartChild =>
-                            if (true) then
-                                targetState <= STATE_SetLED;
-                                internalState <= OnExit;
-                            else
-                                internalState <= Internal;
-                            end if;
+                            targetState <= STATE_SetLED;
+                            internalState <= OnExit;
                         when others =>
                             null;
                     end case;
@@ -228,10 +214,6 @@ process (clk)
                     end case;
                     internalState <= WriteSnapshot;
                 when OnExit =>
-                    case currentState is
-                        when others =>
-                            null;
-                    end case;
                     internalState <= WriteSnapshot;
                 when WriteSnapshot =>
                     EXTERNAL_LED <= LED;
